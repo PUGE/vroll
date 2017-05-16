17d0f4cc1a01a92c3ac084eeaf79bbeb1a321288
蒲鸽 <my@owo.help>
2017-05-16 19:46:03 +0800

    clear

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import IScrollView from './index.vue'
-import IScroll from './iscroll-lite.js'
-export default {
-  install (Vue, options = {}) {
-    Vue._IScroll = IScroll
-    Vue.component('v-roll', IScrollView)
-  }
-}
